4bedc929841221e7c6d2164504c65aff80cd772c
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-02-08 10:12:47 -0800

    week 1 workshop assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -25,4 +25,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send campsite data to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsiteId');
+})
+.delete((req, res) => {
+    res.end('Deleting campsite');
+});
+
 module.exports = campsiteRouter;
